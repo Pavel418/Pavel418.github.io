becbbbb8353d4df95c9112fe27bffc108dba6b23
Pavel418 <pavel@vvv.az>
2024-11-17 11:18:59 +0400

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3345508451"
+    serviceWorkerVersion: "1347783855"
   }
 });
